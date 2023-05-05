14ea7c7c3f8a124704a22794cc36096e5dc8ac75
mr-ristic <marko.risticcc@gmail.com>
2023-05-05 12:47:14 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# NFT RARITY EXCERSISE
+
+## Install
+
+```bash
+npm i
+```
+
+## Run
+
+```bash
+npm run start
+```
+
+### Requirements of the task
+
+### Requirements
+
+1. Your NFT collection should be a “sub-collection” of [Bored Ape NFT collection](https://opensea.io/collection/boredapeyachtclub)
+    1. You should fetch the Bored Apes Yacht Club collection (BAYC) in an array
+    2. Then filter it, to keep only the items having their index a multiple of 100 to form your collection (so you should end up with only 100 items, all indexes ending with '00’.)
+    3. Clean the array from anything you won’t need for this exercise (metadata, etc..) 
+    4. At this stage you only need, for each item, an identifier, and its rarity traits
+2. Create an algorithm to rank your collection by rarity considering that :
+    1. You can’t use any existing rarity ranking dataset, you need to write your own logic with what you have here.
+    2. each rarity traits brings a certain value, and the more rare this trait is amongst the collection, the more value it brings. Figure out a way to give values to this, and appreciate each items value/rarity. Try to achieve this appreciation in the most logic way.
+3. You can only use js (node.js) or php. 
+4. For the data handling, you can use native arrays and json format.
+5. The algorithm should then be able to accept any well formatted similar array of a collection, no matter the number or items.
---
+++ index.js
@@ -0,0 +1,62 @@
+const sdk = require('api')('@reservoirprotocol/v3.0#2l6fslh5fu4vc');
+const API_KEY = '669140fd-f9cb-5e2a-8c37-ab75edbaab17'
+const COLLECTION_ID = '0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d'
+
+async function fetchTokens(offset = 0) {
+  try {
+    sdk.auth(API_KEY)
+    const response = await sdk.getTokensV6({
+      collection: COLLECTION_ID, 
+      limit: '100',
+      includeAttributes: 'true',
+      offset, 
+      accept: '*/*'})
+    return await response.data
+  } catch (error) {
+    console.error(error)
+  }  
+}
+
+const traitFrequency = {};
+
+(async () => {
+  
+  const collection = await fetchTokens()
+  
+  const cleanedArray = collection.tokens.map(token => {
+    const { tokenId, attributes, rarity } = token.token;
+    return {
+      tokenId,
+      rarity,
+      traits: attributes.map(attr => ({ key: attr.key, value: attr.value })),
+    };
+  });
+
+  cleanedArray.forEach(item => {
+    item.traits.forEach(trait => {
+      const key = `${trait.key}:${trait.value}`;
+      traitFrequency[key] = (traitFrequency[key] || 0) + 1;
+    });
+  });
+
+  const calculateRarityScore = (traits) => {
+    let rarityScore = 0;
+    traits.forEach(trait => {
+      const key = `${trait.key}:${trait.value}`;
+      const frequency = traitFrequency[key];
+      rarityScore += (1 / frequency);
+    });
+    return rarityScore;
+  };
+
+  const rankedCollection = cleanedArray.map(item => ({
+    tokenId: item.tokenId,
+    traits: item.traits,
+    rarityScore: calculateRarityScore(item.traits),
+  })).sort((a, b) => b.rarityScore - a.rarityScore);
+
+  console.log(rankedCollection);
+  
+
+})()
+
